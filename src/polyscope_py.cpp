32a4d341c556f89941603dad886585ecbf96da69
Nick Sharp <nsharp@cs.cmu.edu>
2019-10-02 15:07:06 +0200

    point cloud bindings

--- src/polyscope_py.cpp
+++ src/polyscope_py.cpp
@@ -6,9 +6,25 @@
 
 #include "polyscope/polyscope.h"
 #include "polyscope/surface_mesh.h"
+#include "polyscope/point_cloud.h"
 
 namespace py = pybind11;
 namespace ps = polyscope;
+
+// =====================================================================
+// ======================  Point Cloud  ================================
+// =====================================================================
+
+
+ps::PointCloud* register_point_cloud(std::string name, Eigen::MatrixXd points) {
+  if (points.cols() == 2) {
+    return ps::registerPointCloud2D(name, points);
+  } else if (points.cols() == 3) {
+    return ps::registerPointCloud(name, points);
+  } else {
+    throw std::runtime_error("points array must have 2 or 3 columns. has: " + std::to_string(points.cols()));
+  }
+}
 
 // =====================================================================
 // =====================  Surface Mesh  ================================
@@ -35,6 +51,7 @@ PYBIND11_MODULE(polyscope, m) {
   // Basic flow 
   m.def("init", &ps::init, "Initialize Polyscope");
   m.def("show", &ps::show, "Show the Polyscope GUI (blocks until UI is exited)");
+  m.def("remove_all_structures", &ps::removeAllStructures, "Remove all structures from polyscope");
 
   
   // === Enums and other options
@@ -43,6 +60,33 @@ PYBIND11_MODULE(polyscope, m) {
     .value("symmetric", ps::DataType::SYMMETRIC)
     .value("magnitude", ps::DataType::MAGNITUDE)
     .export_values(); 
+  py::enum_<ps::VectorType>(m, "VectorType")
+    .value("standard", ps::VectorType::STANDARD)
+    .value("ambient", ps::VectorType::AMBIENT)
+    .export_values(); 
+ 
+
+  // === Point Cloud
+
+  // Helper classes
+  py::class_<ps::PointCloudColorQuantity>(m, "PointCloudColorQuantity");
+  py::class_<ps::PointCloudScalarQuantity>(m, "PointCloudScalarQuantity");
+  py::class_<ps::PointCloudVectorQuantity>(m, "PointCloudVectorQuantity");
+
+  // Main class, with adder methods
+  py::class_<ps::PointCloud>(m, "PointCloud")
+    .def("remove_all_quantities", &ps::PointCloud::removeAllQuantities, "Remove all quantities")
+    .def("add_color_quantity", &ps::PointCloud::addColorQuantity<Eigen::MatrixXd>, "Add a color function at points",
+        py::arg("name"), py::arg("values"), py::return_value_policy::reference)
+    .def("add_scalar_quantity", &ps::PointCloud::addScalarQuantity<Eigen::VectorXd>, "Add a scalar function at points",
+        py::arg("name"), py::arg("values"), py::arg("data_type")=ps::DataType::STANDARD, py::return_value_policy::reference)
+    .def("add_vector_quantity", &ps::PointCloud::addVectorQuantity<Eigen::MatrixXd>, "Add a vector function at points",
+        py::arg("name"), py::arg("values"), py::arg("vector_type")=ps::VectorType::STANDARD, py::return_value_policy::reference);
+
+  // Static adders and getters
+  m.def("register_point_cloud", &register_point_cloud, "Register a point cloud", py::return_value_policy::reference);
+  m.def("get_point_cloud", &polyscope::getPointCloud, "Get a point cloud by name", py::return_value_policy::reference);
+
 
   // === Surface Mesh
 
@@ -51,7 +95,10 @@ PYBIND11_MODULE(polyscope, m) {
 
   // Main class, with adder methods
   py::class_<ps::SurfaceMesh>(m, "SurfaceMesh")
+    .def("remove_all_quantities", &ps::SurfaceMesh::removeAllQuantities, "Remove all quantities")
     .def("add_vertex_scalar_quantity", &ps::SurfaceMesh::addVertexScalarQuantity<Eigen::VectorXd>, "Add a scalar function at vertices",
+        py::arg("name"), py::arg("values"), py::arg("data_type")=ps::DataType::STANDARD, py::return_value_policy::reference)
+    .def("add_face_scalar_quantity", &ps::SurfaceMesh::addFaceScalarQuantity<Eigen::VectorXd>, "Add a scalar function at faces",
         py::arg("name"), py::arg("values"), py::arg("data_type")=ps::DataType::STANDARD, py::return_value_policy::reference);
 
   // Static adders and getters
--- test/polyscope_demo.py
+++ test/polyscope_demo.py
@@ -27,22 +27,71 @@ def main():
     verts, faces = igl.read_triangle_mesh(args.mesh)
 
 
+    ### Polyscope things example
 
-
-    ### Polyscope things
+    # Always initialize exactly once
     polyscope.init() 
 
-    ps_mesh = polyscope.register_surface_mesh("test mesh", verts, faces)
 
-    # Scalar functions
-    ps_mesh.add_vertex_scalar_quantity("X", verts[:,0])
-    ps_mesh.add_vertex_scalar_quantity("Y", verts[:,1])
+    ## Examples with a mesh
+    if True:
+        ps_mesh = polyscope.register_surface_mesh("test mesh", verts, faces)
 
-    # Look at them
-    polyscope.show() 
-   
-    # Add another one and look again
-    ps_mesh.add_vertex_scalar_quantity("Z", verts[:,2])
+        # Scalar functions
+        ps_mesh.add_vertex_scalar_quantity("X", verts[:,0])
+        ps_mesh.add_vertex_scalar_quantity("Y", verts[:,1])
+
+        # Look at them
+        polyscope.show() 
+
+        # Clear out the quantities
+        ps_mesh.remove_all_quantities()
+
+        # Add another one and look again
+        polyscope.get_surface_mesh("test mesh").add_vertex_scalar_quantity("Z", verts[:,2]) # this time, get mesh by name
+        polyscope.show() 
+        
+        # Remove the whole mesh structure
+        polyscope.remove_all_structures()
+
+
+
+    ## Examples with a mesh
+    if True:
+        ps_points = polyscope.register_point_cloud("test points", verts)
+
+        # Scalar functions
+        ps_points.add_scalar_quantity("X", verts[:,0])
+
+        # Look at them
+        polyscope.show() 
+
+        # Clear out the quantities
+        ps_points.remove_all_quantities()
+
+        # Add more and look again
+        ps_points.add_scalar_quantity("Y", verts[:,1])
+        polyscope.get_point_cloud("test points").add_scalar_quantity("Z", verts[:,2]) # this time, get structure by name
+        polyscope.show() 
+
+        # Add some vectors
+        vecs = np.random.rand(verts.shape[0], 3) - 0.5 
+        polyscope.get_point_cloud("test points").add_vector_quantity("vecs", vecs) 
+
+        # Add some colors
+        colors = np.random.rand(verts.shape[0], 3)
+        polyscope.get_point_cloud("test points").add_color_quantity("colors", colors) 
+    
+        # Show 
+        polyscope.show() 
+
+        # Remove the whole mesh structure
+        polyscope.remove_all_structures()
+
+
+
+
+    # Back to empty
     polyscope.show() 
 
 if __name__ == '__main__':
