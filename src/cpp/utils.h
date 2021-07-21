b74d883739eef57867b6a621a04c37b740df034b
Nicholas Sharp <nsharp@cs.cmu.edu>
2021-07-21 15:10:17 -0400

    fix silly local reference bug

--- src/cpp/utils.h
+++ src/cpp/utils.h
@@ -6,7 +6,7 @@ namespace py = pybind11;
 
 // Add common bindings for all scalar quantities
 template<typename ScalarQ>
-py::class_<ScalarQ>& bindScalarQuantity(py::module& m, std::string name) {
+py::class_<ScalarQ> bindScalarQuantity(py::module& m, std::string name) {
   return py::class_<ScalarQ>(m, name.c_str())
     .def("set_enabled", &ScalarQ::setEnabled, "Set enabled")
     .def("set_color_map", &ScalarQ::setColorMap, "Set color map")
@@ -16,14 +16,14 @@ py::class_<ScalarQ>& bindScalarQuantity(py::module& m, std::string name) {
 
 // Add common bindings for all color quantities
 template<typename ColorQ>
-py::class_<ColorQ>& bindColorQuantity(py::module& m, std::string name) {
+py::class_<ColorQ> bindColorQuantity(py::module& m, std::string name) {
   return py::class_<ColorQ>(m, name.c_str())
     .def("set_enabled", &ColorQ::setEnabled, "Set enabled");
 }
 
 // Add common bindings for all vector quantities
 template<typename VectorQ>
-py::class_<VectorQ>& bindVectorQuantity(py::module& m, std::string name) {
+py::class_<VectorQ> bindVectorQuantity(py::module& m, std::string name) {
   return py::class_<VectorQ>(m, name.c_str())
       .def("set_enabled", &VectorQ::setEnabled, "Set enabled")
       .def("set_length",  &VectorQ::setVectorLengthScale, "Set length")
