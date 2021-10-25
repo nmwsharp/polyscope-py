10cc0e4ea1be22adcc4fc8ba6cae3910c863ee01
Thomas Carey <thomas@thomascarey.us>
2021-10-25 11:22:05 -0400

    Added slice functions

--- src/polyscope/core.py
+++ src/polyscope/core.py
@@ -163,6 +163,12 @@ class SlicePlane:
     
     def get_draw_widget(self):
         return self.bound_slice_plane.get_draw_widget()
+    
+    def set_sliced_volume_mesh(self, val):
+        return self.bound_slice_plane.set_sliced_volume_mesh(val)
+
+    def get_sliced_volume_mesh(self):
+        return self.bound_slice_plane.get_sliced_volume_mesh()
 
 def add_scene_slice_plane():
     instance = psb.add_scene_slice_plane(False)
