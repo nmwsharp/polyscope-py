c6b8e428b2fd57b9220a8d1cbe4fd0b7148ae610
Wesley Smith <wesley.hoke@gmail.com>
2021-11-25 23:31:40 -0600

    fix compilation

--- src/cpp/core.cpp
+++ src/cpp/core.cpp
@@ -7,9 +7,14 @@
 #include "Eigen/Dense"
 
 #include "polyscope/affine_remapper.h"
+#include "polyscope/curve_network.h"
+#include "polyscope/pick.h"
+#include "polyscope/point_cloud.h"
 #include "polyscope/polyscope.h"
+#include "polyscope/surface_mesh.h"
 #include "polyscope/surface_parameterization_enums.h"
 #include "polyscope/view.h"
+#include "polyscope/volume_mesh.h"
 
 namespace py = pybind11;
 namespace ps = polyscope;
