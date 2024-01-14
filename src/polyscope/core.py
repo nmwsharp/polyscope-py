0b0507e5627624bb5e939bbf5d3b2a94ae5316de
Abhishek Madan <abhimadan@users.noreply.github.com>
2024-01-14 00:18:43 -0500

    Correctly reshape generate_camera_rays output (#35)
    Co-authored-by: Nicholas Sharp <nmwsharp@gmail.com>

--- src/polyscope/core.py
+++ src/polyscope/core.py
@@ -466,7 +466,7 @@ class CameraParameters:
                 int(dims[0]), int(dims[1]),
                 str_to_image_origin(image_origin)
             )
-        return out_rays.reshape(dims[0], dims[1], 3)
+        return out_rays.reshape(dims[1], dims[0], 3)
 
     def generate_camera_ray_corners(self): 
         return self.instance.generate_camera_ray_corners()
