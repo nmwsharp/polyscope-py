36d05800175ce7285c510ea2c80d92c20b30e293
Nicholas Sharp <nmwsharp@gmail.com>
2025-12-14 02:14:42 -0800

    update for new callback namespace

--- src/cpp/core.cpp
+++ src/cpp/core.cpp
@@ -146,8 +146,8 @@ PYBIND11_MODULE(polyscope_bindings, m) {
   m.def("set_display_message_popups", [](bool x) { ps::options::displayMessagePopups = x; });
   m.def("set_configure_imgui_style_callback", [](std::function<void()> x) { ps::options::configureImGuiStyleCallback = x; });
   m.def("clear_configure_imgui_style_callback", []() {ps::options::configureImGuiStyleCallback = polyscope::configureImGuiStyle;});
-  m.def("set_files_dropped_callback", [](std::function<void(const std::vector<std::string>&)> x) { ps::options::filesDroppedCallback = x; });
-  m.def("clear_files_dropped_callback", []() {ps::options::filesDroppedCallback = nullptr;});
+  m.def("set_files_dropped_callback", [](std::function<void(const std::vector<std::string>&)> x) { ps::state::filesDroppedCallback = x; });
+  m.def("clear_files_dropped_callback", []() {ps::state::filesDroppedCallback = nullptr;});
 
 
   // === Scene extents
